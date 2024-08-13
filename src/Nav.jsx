fc82856d78798d6aa5388f569a668835b67905aa
Andy Le <4ndyle@gmail.com>
2024-08-12 22:24:49 -0700

    DOCUMENT: Add personal logo to browser elements

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,7 +2,7 @@ function Nav() {
   return (
     <section className="fixed top-0 left-0 w-full h-[57px] px-6 py-3 border-b border-[#606060] justify-between items-center flex bg-black z-[10]">
       <div className="text-white text-base font-normal font-['Inter'] leading-snug">
-        Andy Le
+        <img src="4L.png" className="h-[50px] apsect-square" />
       </div>
       <div className="h-[45px] justify-start items-center gap-[50px] flex">
         <div className="w-[30px] h-[45px] flex-col justify-center items-center inline-flex">
