833396a9bca719e15637c1e2a45543754e04053c
Andy Le <4ndyle@gmail.com>
2024-08-19 23:18:29 -0700

    document: remove unused import statements

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import Nav from "./components/Nav.jsx";
 import Home from "./HomePage.jsx";
-import ScrollChangeBackground from "./components/ScrollChangeBackground.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
