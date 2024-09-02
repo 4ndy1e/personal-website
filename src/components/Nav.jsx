e5c45b6b1cf70c9913eb54b6993a9707d9282dab
Andy Le <4ndyle@gmail.com>
2024-09-02 01:28:25 -0700

    document: remove unecessary code and imrpove readability

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,31 +2,39 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <section className="fixed top-0 left-0 w-full h-[57px] px-6 py-3 border-b border-[#606060] justify-between items-center flex bg-black z-[10]">
+    <section className="fixed top-0 left-0 w-full h-[57px] px-6 py-3 border-b border-[#606060] justify-between items-center flex bg-black z-[10] max-sm:h-[45px]">
       <div className="text-white text-base font-normal font-['Inter'] leading-snug">
-        <img src="4L.png" className="h-[50px] apsect-square" />
+        <img src="4L.png" className="h-[50px] apsect-square max-sm:h-[35px]" />
       </div>
-      <div className="h-[45px] justify-start items-center gap-[50px] flex">
-        <div className="w-[30px] h-[45px] flex-col justify-center items-center inline-flex">
+      <div className="h-[45px] justify-start items-center gap-16 flex max-sm:gap-8">
+        <div className="flex-col justify-center items-center">
           <Link
             to="/"
             className={window.location.pathname === "/" ? "active" : ""}
           >
-            <img className="w-[30px] h-[30px]" src="work.png" alt="Work" />
+            <img
+              className="h-[30px] aspect-square max-sm:h-[25px]"
+              src="work.png"
+              alt="Work"
+            />
           </Link>
         </div>
-        <div className="w-[30px] h-[45px] flex-col justify-center items-center inline-flex">
+        <div className="flex-col justify-center items-center">
           <Link
             to="/about"
             className={window.location.pathname === "/about" ? "active" : ""}
           >
-            <img className="w-[30px] h-[30px]" src="profile.png" alt="About" />
+            <img
+              className="h-[30px] aspect-square max-sm:h-[25px]"
+              src="profile.png"
+              alt="About"
+            />
           </Link>
         </div>
-        <div className="w-[30px] h-[45px] flex-col justify-center items-center inline-flex">
+        <div className="flexjustify-center items-center">
           <a href="https://www.linkedin.com/in/4ndyle/" target="_blank">
             <img
-              className="w-[30px] h-[30px]"
+              className="h-[30px] aspect-square max-sm:h-[25px]"
               src="linkedin.png"
               alt="LinkedIn"
             />
