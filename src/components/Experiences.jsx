e52d271991684f9c2bcc81c9b394eee174771f94
Andy Le <4ndyle@gmail.com>
2024-09-02 01:13:12 -0700

    style: change responsivness of experience section

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -2,7 +2,7 @@ import BlurFade from "./BlurFade";
 
 function Experiences() {
   return (
-    <div className="my-96 flex flex-col w-full">
+    <div className="my-92 flex flex-col w-full">
       <h2 className="text-7xl font-bold my-16 text-left max-md:text-6xl max-md:text-center max-[550px]:text-5xl">
         Experience.
       </h2>
@@ -47,15 +47,15 @@ function Experiences() {
 
 function Work({ name, role, date }) {
   return (
-    <div className="flex justify-between my-12">
-      <div className="flex flex-col text-left">
+    <div className="flex justify-between my-12 max-[550px]:flex-col max-[550px]:gap-4">
+      <div className="flex flex-col text-left max-[550px]:text-center">
         <h5>{name}</h5>
         <p className="text-[#B4B4B4] max-md:text-sm max-[550px]:text-xs">
           {role}
         </p>
       </div>
       <div>
-        <p className="text-xl text-[#B4B4B4] max-md:text-base font-['Inter'] max-[550px]:text-xs">
+        <p className="text-xl font-extralight max-md:text-base font-['Inter'] max-[550px]:text-sm">
           {date}
         </p>
       </div>
